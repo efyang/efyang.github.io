943d4c61fa222600dbc9b8be558b1fe2002c2a3c
Edward Yang <edward.yang6771@gmail.com>
2019-03-21 23:09:55 -0500

    Work on posts generation

--- _generator/src/generator.py
+++ _generator/src/generator.py
@@ -4,7 +4,7 @@ import sys
 
 templateLoader = jinja2.FileSystemLoader(searchpath=sys.argv[1])
 templateEnv = jinja2.Environment(loader=templateLoader)
-TEMPLATE_FILE = "index.html"
+TEMPLATE_FILE = sys.argv[2]
 template = templateEnv.get_template(TEMPLATE_FILE)
 outputText = template.render()  # this is where to put args to the template renderer
 
